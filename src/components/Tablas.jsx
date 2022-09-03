0468ffe73c46369fa39aa04d700ea963012985ca
Jhon <caicedojhon957@gmail.com>
2022-09-03 11:22:29 -0500

    estilo de la nuevas funciones

--- src/components/Tablas.jsx
+++ src/components/Tablas.jsx
@@ -13,17 +13,27 @@ const Tablas = () => {
 
   const {cantidad} = useContext(UserContext);
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [estado, setEstado] = useState()
   
   const consumoApi = async() => {
 
     try {
       const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
       const datos = await res.json()
-      // console.log(datos)
+      console.log(datos)
       setData(datos)
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  const estadoDelPersonaje = () => {
+
+    if(data.gender == 'Female'){
+      setEstado('text-primary')
+    }else {
+      setEstado('text-success')
     }
   }
 
@@ -37,6 +47,7 @@ const Tablas = () => {
 
   useEffect(() => {
     consumoApi()
+    estadoDelPersonaje()
   },[])
   return (
     <div className='container mt-3'>
@@ -44,8 +55,34 @@ const Tablas = () => {
       <hr />
       {id}
 
-      <div className='contenedor shadow'>
-        <div className='bara'></div>
+      <div className="contenido">
+        <div className="personaje">
+          <div className='contenedor-imagen'>
+            <img className='imagen-personaje shadow' src={data.image} alt="" />
+          </div>
+            <h4>{data.name}</h4>
+          <div className='contenido-parrafo'>
+            <h3 className={estado}>Genero : {data.gender}</h3>
+            <h3>Estado : {data.status}</h3>
+          </div>
+          <h5>Especie : {data.species}</h5>
+        </div>
+        <div className='contenedor shadow'>
+        {
+          item.map(items => {
+            console.log(items)
+            return  <div className='bara shadow'>
+                      <div className="posicion-izquierda">
+                        <img className='imagen' src={data.image} alt="" />
+                        <p className='name'>{cantidad}</p>
+                      </div>
+                      <div className="botones shadow">
+                        <button>Eliminar</button>
+                      </div>
+                    </div>
+          })
+        }
+        </div>
       </div>
     </div>
   )
